837878b6b927354b195e4f953cc9cf8de37b3753
culkid1 <prashanth.petrolpump@gmail.com>
2021-11-14 21:15:38 +0530

    apps

---
+++ .mule/plugins-tmp/telegram-messanger-api-1.0.0-mule-plugin/javac.sh
@@ -0,0 +1,1 @@
+/bin/sh -c cd /var/lib/jenkins/workspace/Connectivity/Connectivity-Tooling/generic-module-deploy && /var/lib/jenkins/tools/hudson.model.JDK/Oracle_JDK_1.8_latest_/jre/../bin/javac @/var/lib/jenkins/workspace/Connectivity/Connectivity-Tooling/generic-module-deploy/target/classes/org.codehaus.plexus.compiler.javac.JavacCompiler866378274926037257arguments -J-Xmx512m
